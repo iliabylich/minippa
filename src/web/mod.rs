e47230065d2b447181e6c5011b54cf8b9ed72c3c
Ilya Bylich <ibylich@gmail.com>
2025-07-06 19:08:24 +0200

    add /install.sh

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -26,6 +26,7 @@ impl Web {
         let app = Router::new()
             .route("/upload", post(upload))
             .route("/status", get(status))
+            .route("/install.sh", get(install_sh))
             .fallback_service(ServeDir::new(Config::dir()))
             .with_state(AppState::new(index));
 
@@ -45,7 +46,6 @@ impl Web {
     }
 }
 
-#[axum::debug_handler]
 async fn upload(
     headers: HeaderMap,
     State(state): State<AppState>,
@@ -106,4 +106,41 @@ fn ensure_filename_is_just_a_filename(filename: &str) -> Result<()> {
 
 async fn status() -> &'static str {
     "OK"
+}
+
+async fn install_sh(headers: HeaderMap) -> String {
+    let Some(host) = headers.get("Host") else {
+        return r#"echo "no Host header given""#.to_string();
+    };
+    let Ok(host) = host.to_str() else {
+        return r#"echo "non-utf8 Host header""#.to_string();
+    };
+
+    let protocol = if cfg!(debug_assertions) {
+        "http"
+    } else {
+        "https"
+    };
+
+    let url = format!("{protocol}://{host}");
+    let gpg_install_path = "/etc/apt/trusted.gpg.d/minippa.gpg";
+
+    format!(
+        r#"set -euo pipefail
+
+echo "Installing mini PPA {url}"
+
+echo "Installing GPG key"
+curl --silent "{url}/public.gpg" | gpg --dearmor | tee "{gpg_install_path}" > /dev/null
+
+echo "Installing APT sources file"
+cat << EOF > /etc/apt/sources.list.d/minippa.sources
+Types: deb
+URIs: {url}/
+Suites: ./
+Components:
+Signed-By: {gpg_install_path}
+EOF
+"#
+    )
 }
